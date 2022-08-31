2f6a4fe39971bd530c0a0021f6001dd8cd2fd0c3
miss-muasya <sheemuasya@gmail.com>
2022-08-31 15:11:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,88 @@
 /* Your Code Here */
+let createEmployeeRecord = (el) => {
+    return {
+        "firstName": el[0],
+        "familyName": el[1],
+        "title": el[2],
+        "payPerHour": el[3],
+        "timeInEvents": [],
+        "timeOutEvents": []
+    };
+}
+
+let createEmployeeRecords = (mainArray) => {
+    let newArr = [];
+    mainArray.forEach(element => {
+        newArr.push(createEmployeeRecord(element));
+    });
+    return (newArr);
+}
+
+let createTimeInEvent = function (dateStmp) {
+    let arr = dateStmp.split(" ");
+    this.timeInEvents.push({
+        "type": "TimeIn",
+        "hour": parseInt(arr[1]),
+        "date": `${arr[0]}`
+    })
+    return this;
+}
+
+let createTimeOutEvent = function (dateStmp) {
+    let arr = dateStmp.split(" ");
+    this.timeOutEvents.push({
+        "type": "TimeOut",
+        "hour": parseInt(arr[1]),
+        "date": `${arr[0]}`
+    })
+    return this;
+}
+
+let hoursWorkedOnDate = function (dateStmp) {
+    let timeIn;
+    let timeOut;
+
+    this.timeOutEvents.forEach((el) => {
+        if (el.date === dateStmp && el.type === "TimeOut") {
+            timeOut = parseInt(el.hour);
+        }
+    })
+
+    this.timeInEvents.forEach((el) => {
+        if (el.date === dateStmp && el.type === "TimeIn") {
+            timeIn = parseInt(el.hour);
+        }
+    })
+    let hrs = parseInt(timeOut - timeIn)
+    return (hrs / 100);
+}
+
+
+let wagesEarnedOnDate = function (dateStmp) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, dateStmp)
+}
+
+
+function allWagesFor() {
+    let total = 0;
+    this.timeInEvents.forEach((i) => {
+        total = total + (wagesEarnedOnDate.call(this, i.date))
+    })
+    return total;
+}
+
+function findEmployeeByFirstName(srcArray, frstName) {
+    return srcArray.find((e) => e.firstName === frstName)
+}
+
+function calculatePayroll(arr) {
+    let total = 0
+    arr.forEach((indx) => {
+        total = total + allWagesFor.call(indx)
+    });
+    return total-1200;
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -9,7 +93,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+/*const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -19,5 +103,5 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
-}
+}*/
 
